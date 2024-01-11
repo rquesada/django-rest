f441386345c1922686674d03911491b84da430a7
Roy Quesada <roy.quesada@gmail.com>
2024-01-11 17:42:25 -0600

    DRF - Class based view

---
+++ watchmate/watchlist_app/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from watchlist_app.models import Movie
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    description = serializers.CharField()
+    activate = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.activate = validated_data.get('activate', instance.activate)
+        instance.save()
+        return instance
+    
