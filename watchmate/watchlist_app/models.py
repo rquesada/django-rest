aca33a459276ecbd002ed154f76588dd7d07f3a2
Roy Quesada <roy.quesada@gmail.com>
2024-01-11 11:30:45 -0600

    Initial project

---
+++ watchmate/watchlist_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    activate = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
